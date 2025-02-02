da452f4f9edab46b7857dcde1cad290aa531483b
Colin Craig <73488952+ccraig17@users.noreply.github.com>
2025-02-02 15:02:57 -0500

    Changes made: implemented sayHello()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        sayHello();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hi from Craig!");
     }
 
 }
